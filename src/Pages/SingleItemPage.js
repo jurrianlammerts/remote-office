69ab7f6c038eacafd9e43a25e18339c550bbe0e2
Jurrian <jurrianlammerts@hotmail.com>
2020-11-11 12:21:26 +0100

    Added fake data

--- src/Pages/SingleItemPage.js
+++ src/Pages/SingleItemPage.js
@@ -47,19 +47,77 @@ const initialData = {
   lanes: [
     {
       id: 'lane1',
-      title: 'Backlog',
-      cards: [],
+      title: 'Product Backlog',
+      cards: [
+        {
+          id: 'Card1',
+          title: 'Write Blog',
+          description: 'Can AI make memes',
+          label: '30 mins',
+          draggable: false,
+        },
+        {
+          id: 'Card2',
+          title: 'Navigation Menu',
+          description: 'Transfer via NEFT',
+          label: '35 mins',
+          metadata: { sha: 'be312a1' },
+        },
+      ],
     },
-
     {
       id: 'lane2',
       title: 'In Progress',
-      cards: [],
+      cards: [
+        {
+          id: 'Card3',
+          title: 'Default Content Page',
+          description: 'Content for basic content page',
+          label: '15 mins',
+        },
+        {
+          id: 'Card4',
+          title: 'Scene Landing Page',
+          description: 'Get all videos from client A',
+          label: '35 mins',
+        },
+        {
+          id: 'Card5',
+          title: 'Send All Invoices',
+          description: 'Complete before 17 oct.',
+          label: '1 hour',
+        },
+      ],
     },
     {
       id: 'lane3',
       title: 'Completed',
-      cards: [],
+      cards: [
+        {
+          id: 'Card7',
+          title: 'Discuss possible animations',
+          description: 'Fixed before deadline',
+          label: '15 mins',
+        },
+        {
+          id: 'Card8',
+          title: 'Write Copy for Contact page',
+          description: 'Don`t spent to much time on this',
+          label: '5 mins',
+        },
+        {
+          id: 'Card9',
+          title: 'Check with Client C',
+          description: 'Invoices still not paid 🤬',
+          label: '10 mins',
+        },
+        {
+          id: 'Card10',
+          title: 'Grab a coffee',
+          description: 'And chill',
+          label: '1 hour',
+        },
+      ],
     },
   ],
 };
